85f716787f039809de7d99b925a38f49da40d901
ybax01 <youssef1002016@gmail.com>
2024-06-20 21:52:49 +0100

    ...

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main(){
-	char str1[]="\"Programming is like building a multilingual puzzle";
+int main()
+{
+	char str1[] = "\"Programming is like building a multilingual puzzle";
 	puts(str1);
 	return 0;
 }
