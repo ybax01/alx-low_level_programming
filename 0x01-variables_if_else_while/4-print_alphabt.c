ad7463c1611c17e55579da71d221b2cd893893e8
ybax01 <youssef1002016@gmail.com>
2024-06-22 20:25:36 +0100

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,7 +11,7 @@
  */
 int main(void) {
         char letter = 'a';
-
+        
         while (letter <= 'z') {
                 if (letter != 'q' && letter != 'e') {
                         putchar(letter);
@@ -19,6 +19,5 @@ int main(void) {
                 letter++;
         }
         putchar('\n');
-
         return (0);
 }
