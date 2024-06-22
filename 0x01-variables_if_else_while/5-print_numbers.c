f6441f68a13c3efb4264ce393578848c6af05315
ybax01 <youssef1002016@gmail.com>
2024-06-22 20:17:19 +0100

    c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,21 +3,22 @@
 /**
  * main - Entry point
  *
- * Description: Prints all single digit numbers of base 10
- *              starting from 0, followed by a new line.
+ * Description:
+ *   Prints all single digit numbers of base 10 starting from 0,
+ *   followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int number = 0;
+	int number = 0;
 
-    while (number < 10)
-    {
-        putchar(number + '0');
-        number++;
-    }
-    putchar('\n');
+	while (number < 10)
+	{
+		putchar(number + '0');
+		number++;
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
