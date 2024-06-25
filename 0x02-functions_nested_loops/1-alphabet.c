95ec0dd5a2455515e58d55d3d144a73cdb5fb0b5
ybax01 <youssef1002016@gmail.com>
2024-06-25 17:59:14 +0100

    c2

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 
 /**
  * main - check the code
+ * print_alphabet - prints the entire alphabet in lowercase
  *
  * Return: Always 0.
  */
  
 void print_alphabet(void)
 {
-	char c[]="abcdefghijklmnopqrstuvwxyz\n";
-	int i=0;
+	char c[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int i = 0;
 
 	while (c[i] != '\0')
 	{
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,16 @@
 #include "main.h"
+#include "1-alphabet.c"
 
 /**
  * main - check the code.
+ * print_alphabet_x10 - print the alphabet 10 times
  *
  * Return: Always 0.
  */
  
 void print_alphabet_x10(void)
 {
-	int j=0;
+	int j = 0;
 	while(j != 10)
 	{
 		print_alphabet();
