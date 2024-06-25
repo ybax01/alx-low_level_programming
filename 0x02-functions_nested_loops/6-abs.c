8726c7f61ce0574b559b707a173f426a5f623944
ybax01 <youssef1002016@gmail.com>
2024-06-25 19:47:20 +0100

    c2

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - Returns the absolute value of the parameter
+ * @n: The number in question
+ *
+ * Return: the absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
