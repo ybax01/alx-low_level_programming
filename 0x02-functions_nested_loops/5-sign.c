666dc14ffc50c5ad4d3b0134d779a4e8e5cc9263
ybax01 <youssef1002016@gmail.com>
2024-06-25 18:41:09 +0100

    c2

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of the number
+ * @n: The checked number
+ *
+ * Return: 1,0,-1 if positive, zero or negative respectively
+ */
+int print_sign(int n)
+{
+	if (i > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (i == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
