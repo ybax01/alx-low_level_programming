07d45f6a4458e97dbf0943e87c31bbf18d53f774
ybax01 <youssef1002016@gmail.com>
2024-06-25 16:01:17 +0100

    c2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,16 @@
 #include <unistd.h>
 
 int _putchar(char c);
+void print_alphabet(void);
+
+void print_alphabet(void)
+{
+	char c='a';
+
+	while (c=!('z'+1))
+	{
+		_putchar(c);
+		c[0]=c[0]+1;
+	}
+	_putchar('\n');
+}
