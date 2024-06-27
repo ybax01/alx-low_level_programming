a699f677f6026f592749f30e8944ea11371139ca
ybax01 <youssef1002016@gmail.com>
2024-06-27 18:59:54 +0100

    C nested loops and functions project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiply the two parameters
+ * @a: 1st num
+ * @b: 2nd num
+ *
+ * Return: sum
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print numbers from 0 to 9
+ *
+ * Return: Nothing
+ */
+void print_numbers(void)
+{
+	char c = '0';
+	
+	while (c <= '9')
+	{
+		_putchar(c);
+	}
+	c = '\n';
+	_putchar(c);
+}
