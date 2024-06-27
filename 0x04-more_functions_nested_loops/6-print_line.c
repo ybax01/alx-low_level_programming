8e2f008314089eba76cbaa0686112c540d60e79f
ybax01 <youssef1002016@gmail.com>
2024-06-27 22:45:45 +0100

    C nested loops and functions project

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n: number of times to print '_'
+ *
+ * Return: Nothing
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
