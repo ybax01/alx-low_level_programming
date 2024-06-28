8df0b25d7475aecacd568eecf71abc119021e2e9
ybax01 <youssef1002016@gmail.com>
2024-06-28 01:38:11 +0100

    C nested loops and functions project

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,26 @@
  */
 void print_diagonal(int n)
 {
+	int i = 0;
+	int j = 0;
+
 	if (n <= 0)
 	{
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\');
+			_putchar('\n');
+			i++;
+		}
 	}
 }
