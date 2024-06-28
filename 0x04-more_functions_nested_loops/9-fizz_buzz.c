90fdb9b8b9626f975d24ddabc297f160edddfcbb
ybax01 <youssef1002016@gmail.com>
2024-06-28 02:04:00 +0100

    C nested loops and functions project

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,24 +9,24 @@ int main(void)
 {
 	int i = 0;
 
-	while (i < 100)
+	while (i <= 100)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
 			printf("FizzBuzz ");
-			continue;
 		}
 		else if (i % 3 == 0)
 		{
 			printf("Fizz ");
-			continue;
 		}
 		else if (i % 5 == 0)
 		{
 			printf("Buzz ");
-			continue;
 		}
-		printf("%d ", i);
+		else
+		{
+			printf("%d ", i);
+		}
 		i++;
 	}
 	return (0);
