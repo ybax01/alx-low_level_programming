792215cf20b9fa29e5eb94088badaea015a2025d
ybax01 <youssef1002016@gmail.com>
2024-07-01 13:50:45 +0100

    cp 01

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,24 +8,24 @@
  */
 void rev_string(char *s)
 {
-    int length = 0;
-    int start = 0;
-    char temp;
+	int length = 0;
+	int start = 0;
+	char temp;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
-    int end = length - 1;
+	int end = length - 1;
 
-    while (start < end)
-    {
-        temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
+	while (start < end)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
 
-        start++;
-        end--;
-    }
+		start++;
+		end--;
+	}
 }
