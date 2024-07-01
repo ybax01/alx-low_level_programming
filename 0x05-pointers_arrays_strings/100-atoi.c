a64abb08259bd2f4f32a695a3554df1bf4345aaf
ybax01 <youssef1002016@gmail.com>
2024-07-02 00:17:04 +0100

    cp 01

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -24,7 +24,7 @@ int _atoi(char *s)
 		{
 
 		}
-		else if (s[i] >= '0' || s[i] <= '9')
+		else if (s[i] >= '0' && s[i] <= '9')
 		{
 			found = 1;
 			result = (result * 10) + (s[i] - '0');
@@ -33,6 +33,7 @@ int _atoi(char *s)
 		{
 			break;
 		}
+		i++;
 	}
 	return (result);
 }
