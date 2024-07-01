79ff6b6e0256148df7110764230cb5571d04eaa3
ybax01 <youssef1002016@gmail.com>
2024-07-01 23:34:03 +0100

    cp 01

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "2-strlen.h"
+
+/**
+ * puts_half - prints 2nd half of the string
+ * @str: adress of string
+ *
+ */
+void puts_half(char *str)
+{
+	int n = _strlen(str);
+	int i;
+
+	if (n % 2 == 0)
+	{
+		i = n / 2;
+	}
+	else
+	{
+		i = (n + 1) / 2
+	}
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
