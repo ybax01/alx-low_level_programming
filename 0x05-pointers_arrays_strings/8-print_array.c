b43f356a07de75cc9254bfaed247f3ea0eda6de5
ybax01 <youssef1002016@gmail.com>
2024-07-01 23:44:33 +0100

    cp 01

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,10 +1,23 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- *
+ * print_array - prints n elements of array with ',' in between
+ * @a: The array
+ * @n: num of elements to print
  *
  */
 void print_array(int *a, int n)
 {
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
 }
