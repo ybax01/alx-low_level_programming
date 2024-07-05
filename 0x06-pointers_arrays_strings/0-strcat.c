390d31d7cb53c561587eedf786db2d8a51db9cd0
ybax01 <youssef1002016@gmail.com>
2024-07-05 13:11:21 +0100

    aze

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,3 +1,25 @@
 #include "main.h"
 
+/**
+ * _strcat - Concatenates two strings
+ * @dest: destination
+ * @src: source
+ * Return: New string
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
 
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		src++;
+		ptr++;
+	}
+	*ptr = '\0';
+	return (dest);
+}
