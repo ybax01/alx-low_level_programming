6cb9ab916c92aa9d37cd5d47aa6a58f4ad8f1385
ybax01 <youssef1002016@gmail.com>
2024-07-10 01:01:26 +0100

    7th

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string
+ * @c: The char
+ *
+ * Return: Pointer to 1st occurence if found, NULL if not
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
+	}
+	return (NULL);
+}
