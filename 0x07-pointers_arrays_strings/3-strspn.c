3837c0d04efa36190e11f34c3045b59a48053919
ybax01 <youssef1002016@gmail.com>
2024-07-10 01:21:31 +0100

    7th

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,16 +7,6 @@
  *
  * Return: The number of bytes
  */
-#include "main.h"
-
-/**
- * _strspn - Gets the length of a prefix substring.
- * @s: The string to be scanned.
- * @accept: The string containing characters to match in s.
- *
- * Return: The number of bytes in the initial segment of s which consist
- *         only of bytes from accept.
- */
 unsigned int _strspn(char *s, char *accept)
 {
 	char *p;
