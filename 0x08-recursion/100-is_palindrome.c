236475bcbefc22bc50d03b05cd9793c9d9c6c3f9
ybax01 <youssef1002016@gmail.com>
2024-07-12 01:44:58 +0100

    recursion learning

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include "2-strlen_recursion.c"
+
+/**
+ * is_palindrome - Checks if a string is a palindrome recursively.
+ * @s: Pointer to the string
+ *
+ * Return: 1 palindrome, 0 otherwise.
+ */
+int is_palindrome(char *s)
+{
+    int l = _strlen(s);
+    return _pldchecker(s, 0, l - 1);
+}
+
+/**
+ * _is_palindrome - Palindrome helper function
+ * @s: Pointer to the string.
+ * @left: Left index
+ * @right: Right index
+ *
+ * Return: 1 if palindrome, 0 otherwise.
+ */
+int _pldchecker(char *s, int left, int right)
+{
+    if (left >= right)
+        return 1;
+
+    if (s[left] != s[right])
+        return 0;
+
+    return _pldchecker(s, left + 1, right - 1);
+}
