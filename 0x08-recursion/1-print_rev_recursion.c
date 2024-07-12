fdcc461a445d5662cc5d25916225fb539a01234a
ybax01 <youssef1002016@gmail.com>
2024-07-12 01:08:14 +0100

    recursion learning

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,8 +1,5 @@
 #include "main.h"
 
-void _prr(char *s);
-
-
 /**
  * _print_rev_recursion - prints a string in reverse
  * @s: pointer to string
@@ -10,24 +7,13 @@ void _prr(char *s);
 
 void _print_rev_recursion(char *s)
 {
-	_prr(s);
-	_putchar('\n');
-}
-
-/**
- * _prr - main part of the function
- * @s: pointer to string
- *
- */
-void _prr(char *s)
-{
 	if (*s == '\0')
 	{
 		return;
 	}
 	else
 	{
-		_prr(s + 1);
+		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
 }
