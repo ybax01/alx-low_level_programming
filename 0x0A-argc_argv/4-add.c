8680440b5b810d1a408d676745930ba83a01bd26
ybax01 <youssef1002016@gmail.com>
2024-07-13 17:32:41 +0100

    arguments stuff...

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,13 +10,25 @@
  */
 int main(int argc, char **argv)
 {
-	int i = 0;
+	int i, j;
 	int sum = 0;
 
 	if (argc < 2)
 	{
-		printf("Error\n");
-		return (1);
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' && argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 	}
 
 	for (i = 1; i < argc; i++)
