4d0a25d1f5504a83e68001e02602cad0e665b91f
ybax01 <youssef1002016@gmail.com>
2024-07-13 17:04:32 +0100

    arguments stuff...

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: arguments count
+ * @argv: arguments array
+ * Return: 0 if success
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
