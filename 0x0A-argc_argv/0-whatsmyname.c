0e01aaf04de7883ed53dd14518878713a3001736
ybax01 <youssef1002016@gmail.com>
2024-07-13 17:02:37 +0100

    arguments stuff...

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,8 +5,9 @@
  * @argc: arguments count
  * @argv: arguments array
  */
-void main(int argc, char **argv)
+int main(int argc, char **argv)
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
+	return (0);
 }
