c9450c04dd9221dfda7357d1e01fbdfc76e45779
ybax01 <youssef1002016@gmail.com>
2024-07-15 17:04:26 +0100

    ma

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,2 +1,21 @@
 #include "main.h"
 
+/**
+ * create_array - create an arrat with mem allocation
+ * @size: size of the array
+ * @c: character to initialize
+ * Return: pointer to array, or NULL if error
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+
+	char *p;
+
+	p = malloc(size * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+
+	*p = c;
+}
