643e0687287c4973aba1abf2e94b581cf9fdbca0
ybax01 <youssef1002016@gmail.com>
2024-07-16 22:51:29 +0100

    ma

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,6 +12,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *p;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
@@ -20,6 +21,8 @@ char *create_array(unsigned int size, char c)
 	if (p == NULL)
 		return (NULL);
 
-	*p = c;
+	for (i = 0; i < size; i++)
+		p[i] = c;
+
 	return (p);
 }
