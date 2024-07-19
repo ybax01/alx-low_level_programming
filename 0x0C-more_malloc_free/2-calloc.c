36f277a4a959b52d5955591bd07ebb12352dbbbd
ybax01 <youssef1002016@gmail.com>
2024-07-19 18:46:18 +0100

    smth

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -5,6 +5,7 @@
  * _calloc - allocates a memory for an array
  * @nmemb: numb of blocks
  * @size: block size
+ * Return: null if error or parameters are 0, otherwise return the pointer needed
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
