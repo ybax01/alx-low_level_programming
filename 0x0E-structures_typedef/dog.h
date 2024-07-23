52e4d60cfd7b16e68c95f1629e96cf797381c2d6
ybax01 <youssef1002016@gmail.com>
2024-07-23 23:18:23 +0100

    hmm

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - represents a dog
@@ -5,9 +7,12 @@
  * @age: age
  * @owner: owner
  */
+
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
+
+#endif /* DOG_H */
