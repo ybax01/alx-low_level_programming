4285f503504e4813465a69b0961cea4ef0844734
ybax01 <youssef1002016@gmail.com>
2024-07-24 22:19:38 +0100

    fp c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+/**
+ * int_index - Searches for an integer in an array
+ * @array: The array to search
+ * @size: The number of elements in the array
+ * @cmp: Pointer to the function to compare values
+ * 
+ * Return: The index of the first matching element, or -1 if no match or error
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array && cmp && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
+
