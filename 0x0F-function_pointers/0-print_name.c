ede1c6e1a2804d264c0d06a4e7d9912d29239850
ybax01 <youssef1002016@gmail.com>
2024-07-24 22:12:22 +0100

    fp c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ * print_name - Prints a name using a given function
+ * @name: The name to print
+ * @f: Pointer to the function to use
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
