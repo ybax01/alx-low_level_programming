69a969d8fa102d96e27d8caa908b74638d2d3607
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-08-16 20:46:45 +0100

    files stuff

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,50 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- * read_textfile - prints the text file it reads and prints it the output
- * @filename: self-explanatory
- * @letters: num of letters to read
+ * read_textfile - Reads a text file and prints it to the POSIX standard output
+ * @filename: Name of the file to read
+ * @letters: Number of letters to read and print
+ *
+ * Return: The number of letters it could read and print, or 0 if error
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd;
-        ssize_t r_count, w_count;
-        char *buffer;
+	int fd;
+	ssize_t r_count, w_count;
+	char *buffer;
 
-        if (filename == NULL)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        buffer = malloc(letters);
-        if (buffer == NULL)
-                return (0);
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-        {
-                free(buffer);
-                return (0);
-        }
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buffer);
+		return (0);
+	}
 
-        r_count = read(fd, buffer, letters);
-        if (r_count == -1)
-        {
-                close(fd);
-                free(buffer);
-                return (0);
-        }
+	r_count = read(fd, buffer, letters);
+	if (r_count == -1)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-        w_count = write(STDOUT_FILENO, buffer, r_count);
-        if (w_count == -1 || w_count != r_count)
-        {
-                close(fd);
-                free(buffer);
-                return (0);
-        }
+	w_count = write(STDOUT_FILENO, buffer, r_count);
+	if (w_count == -1 || w_count != r_count)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
 
-        close(fd);
-        free(buffer);
-        return (r_count);
+	close(fd);
+	free(buffer);
+	return (r_count);
 }
+
---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: self-explanatory
+ * @text_content: null terminated string to write to the file
+ * Return: 
+ */
+int create_file(const char *filename, char *text_content)
