bb7ab13a2b292ce38965f5902f24a2511b1a1ae8
Youssef Ben Ayad <youssef1002016@gmail.com>
2024-10-10 16:32:53 +0100

    az

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -78,3 +78,34 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	return (1);
 }
+
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table.
+ * @key: The key to look for.
+ *
+ * Return: The value associated with the key, or NULL if not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	node = ht->array[index];
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+		node = node->next;
+	}
+
+	return (NULL);
+}
