16acd45e2d30e0fdd02d9f256cd51b96dbf262cd
Youssef Ben Ayad <youssef1002016@gmail.com>
2025-01-29 12:29:45 +0100

    az

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,14 +12,14 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i = 0;
+	size_t i = 0;
 
 	if (!array)
 		return (-1);
 
 	while (i < size)
 	{
-		printf ("Value checked array[%d] = %d", i, array[i]);
+		printf ("Value checked array[%ld] = %d", i, array[i]);
 		if (array[i] == value)
 			return (i);
 		else
